1d163c2e26dbf8d5e718eaf73bbbdc526ad6d4d9
佐々木亮 <@LAPTOP-MNGCPP1G>
2020-05-15 15:25:08 +0900

    6章で作成

--- sprintSampleNew/src/main/java/com/example/demo/login/controller/SignupController.java
+++ sprintSampleNew/src/main/java/com/example/demo/login/controller/SignupController.java
@@ -9,10 +9,12 @@ import java. util. Map;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 
+import com.example.demo.login.domain.model.GroupOrder;
 import com.example.demo.login.domain.model.SignupForm;
 
 /**
@@ -37,7 +39,7 @@ public class SignupController {
 	// ポイント1 : @ModelAttribute
 	//ユーザー登録画面のGET用コントローラー.
 	@GetMapping("/signup")
-	public String getSignUp(@ModelAttribute SignupForm form, Model model) {
+	public String getSignUp(@ModelAttribute SignupForm form, BindingResult bindingResult, Model model) {
 		//ラジオボタンの初期化メソッド呼び出し
 		radioMarriage = initRadioMarrige();
 		//ラジオボタン用のMapをModelに登録
@@ -49,12 +51,12 @@ public class SignupController {
 	//ユーザー登録画面のPOST用コントローラー.
 	// ポイント2 : データバインドの結果の受け取り
 	@PostMapping("/signup")
-	public String postSignUp(@ModelAttribute SignupForm form, BindingResult bindingResult, Model model) {
+	public String postSignUp(@ModelAttribute @Validated(GroupOrder.class) SignupForm form, BindingResult bindingResult, Model model) {
 		// ポイント3 : データバインド失敗の場合
 		// 入力チェックに引っかかった場合、ユーザ登録画面に戻る
 		if (bindingResult.hasErrors()) {
 			// GETリクエスト用のメソッドを呼び出して、ユーザー登録画面に戻ります。
-			return getSignUp(form, model);
+			return getSignUp(form, bindingResult, model);
 		}
 
 		// formの内容をコンソールに出して確認する
--- sprintSampleNew/src/main/java/com/example/demo/login/domain/model/SignupForm.java
+++ sprintSampleNew/src/main/java/com/example/demo/login/domain/model/SignupForm.java
@@ -5,6 +5,15 @@ package com.example.demo.login.domain.model;
 
 import java.util.Date;
 
+import javax.validation.constraints.AssertFalse;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.Max;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Pattern;
+
+import org.hibernate.validator.constraints.Length;
 import org.springframework.format.annotation.DateTimeFormat;
 
 import lombok.Data;
@@ -15,12 +24,22 @@ import lombok.Data;
  */
 @Data
 public class SignupForm {
+	@NotBlank(groups = ValidGroup1.class)
+	@Email(groups = ValidGroup2.class)
 	private String userId;		// ユーザー ID
+	@NotBlank(groups = ValidGroup1.class)
+	@Length(min=4, max=100, groups = ValidGroup2.class)
+	@Pattern(regexp = "^[a-zA-Z0-9]+$", groups = ValidGroup3.class)
 	private String password;	// パスワード
+	@NotBlank(groups = ValidGroup1.class)
 	private String userName;	// ユーザー 名
 	// ポイント：@ DateTimeFormat
+	@NotNull(groups = ValidGroup1.class)
 	@DateTimeFormat( pattern = "yyyy/MM/dd")
 	private Date birthday;		// 誕生日
+	@Min(value=20, groups = ValidGroup2.class)
+	@Max(value=100, groups = ValidGroup2.class)
 	private int age;			// 年齢
+	@AssertFalse(groups = ValidGroup2.class)
 	private boolean marriage;	// 結婚 ステータス
 }
