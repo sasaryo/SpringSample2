ca1a19866f577b0c94d04721e56c793faa9a07c8
佐々木亮 <@LAPTOP-MNGCPP1G>
2020-05-20 18:02:37 +0900

    12章 更新

---
+++ sprintSampleNew/src/main/resources/data.sql
@@ -0,0 +1,11 @@
+/* 従業員テーブルのデータ（第３章で作成） */
+INSERT INTO employee (employee_id, employee_name, age)
+VALUES(1, '佐々木亮', 41);
+
+/* ユーザーマスタのデータ（ADMIN権限） */
+INSERT INTO m_user (user_id, password, user_name, birthday, age, marriage, role)
+VALUES('sasaki@xxx.co.jp', '$2a$10$xRTXvpMWly0oGiu65WZlm.3YL95LGVV2ASFjDhe6WF4.Qji1huIPa', '佐々木亮', '1978-10-04', 41, false, 'ROLE_ADMIN');
+
+/* ユーザーマスタのデータ（一般権限） */
+INSERT INTO m_user (user_id, password, user_name, birthday, age, marriage, role)
+VALUES('tamura@xxx.co.jp', '$2a$10$xRTXvpMWly0oGiu65WZlm.3YL95LGVV2ASFjDhe6WF4.Qji1huIPa', '田村', '1986-11-05', 31, false, 'ROLE_GENERAL');
